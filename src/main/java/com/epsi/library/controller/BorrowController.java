fedd89fa21c7cfe7ba3aaaa8598906c47a0002df
Clément LO-CASCIO <49551394+ClemLcs@users.noreply.github.com>
2023-06-07 18:35:55 +0200

    feat(borrow): Add method to update a Borrow

--- src/main/java/com/epsi/library/controller/BorrowController.java
+++ src/main/java/com/epsi/library/controller/BorrowController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/borrow")
@@ -20,7 +21,7 @@ public class BorrowController {
     /**
      * Allows to get all borrows
      *
-     * @return ResponseEntity 204 / 200 / 500
+     * @return HTTP code 204 / 200 / 500
      */
     @GetMapping("")
     public ResponseEntity<List<Borrow>> getAll() {
@@ -42,7 +43,7 @@ public class BorrowController {
      * Allows to create a Borrow.
      *
      * @param borrowRequest
-     * @return ResponseEntity 201 / 500
+     * @return HTTP code  201 / 500
      */
     @PostMapping("")
     public ResponseEntity<Borrow> create(@RequestBody Borrow borrowRequest) {
@@ -55,4 +56,32 @@ public class BorrowController {
         }
     }
 
+    /**
+     * Allows to update a borrow
+     *
+     * @param borrowRequest
+     * @return HTTP code 200 / 404 / 500
+     */
+    @PutMapping("")
+    public ResponseEntity<Borrow> update(@RequestBody Borrow borrowRequest) {
+        try {
+            if (borrowRequest.getId() != null) {
+                Optional<Borrow> _borrow = borrowRepository.findById(borrowRequest.getId());
+                if (_borrow.isPresent()) {
+                    Borrow _borrowData = _borrow.get();
+                    _borrowData.setLoan_date(borrowRequest.getLoan_date());
+                    _borrowData.setExpected_end_date(borrowRequest.getExpected_end_date());
+                    _borrowData.setBorrow_end(borrowRequest.getBorrow_end());
+
+                    return new ResponseEntity<>(borrowRepository.save(_borrowData), HttpStatus.OK);
+
+                }
+            }
+
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
