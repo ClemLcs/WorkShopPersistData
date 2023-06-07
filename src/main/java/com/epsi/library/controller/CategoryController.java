e47b2e5efa0486825b262a06d83dc084d9a7756a
Clément LO-CASCIO <49551394+ClemLcs@users.noreply.github.com>
2023-06-07 19:11:14 +0200

    feat(category): Add method to update a category

--- src/main/java/com/epsi/library/controller/CategoryController.java
+++ src/main/java/com/epsi/library/controller/CategoryController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/category")
@@ -49,6 +50,33 @@ public class CategoryController {
             Category _category = categoryRepository
                     .save(new Category(categoryRequest.getName()));
             return new ResponseEntity<>(_category, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * Allows to update a category
+     *
+     * @param categoryRequest
+     * @return 200 / 404 / 500 HTTP code
+     */
+    @PutMapping("")
+    public ResponseEntity<Category> update(@RequestBody Category categoryRequest) {
+        try {
+            if (categoryRequest.getId() != null) {
+                Optional<Category> _category = categoryRepository.findById(categoryRequest.getId());
+                if (_category.isPresent()) {
+                    Category _categoryData = _category.get();
+                    _categoryData.setName(categoryRequest.getName());
+
+                    return new ResponseEntity<>(categoryRepository.save(_categoryData), HttpStatus.OK);
+
+                }
+            }
+
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
         } catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
