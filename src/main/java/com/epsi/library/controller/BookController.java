980f777bf895188a313e18533b542fe58e1b9de1
Clément LO-CASCIO <49551394+ClemLcs@users.noreply.github.com>
2023-06-09 00:27:48 +0200

    fix(book): As a User I want to show all books

--- src/main/java/com/epsi/library/controller/BookController.java
+++ src/main/java/com/epsi/library/controller/BookController.java
@@ -4,85 +4,39 @@ import com.epsi.library.entity.Book;
 import com.epsi.library.repository.BookRepository;
 import com.epsi.library.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.reactive.function.client.WebClient;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
-@RestController
-@RequestMapping(path = "/book")
+@Controller
+@RequestMapping("/book")
 public class BookController {
     @Autowired
     BookRepository bookRepository;
-
+    @Autowired
+    private WebClient.Builder webClientBuilder;
     @Autowired
     CategoryRepository categoryRepository;
+    @Value("${serverPort}")
+    private String serverPort;
 
-    /**
-     * Allows to get all books save in DB
-     *
-     * @return ResponseEntity 200 / 500
-     */
-    @RequestMapping(path = "/", method = RequestMethod.GET)
-    public ResponseEntity<List<Book>> readAll() {
-        try {
-            List<Book> books = new ArrayList<Book>();
-            bookRepository.findAll().forEach(books::add);
+    @GetMapping("")
+    public String listBooks(Model model) {
+        List<Book> books = webClientBuilder.build()
+                .get()
+                .uri("http://localhost:" + serverPort + "/rest/book")
+                .retrieve()
+                .bodyToFlux(Book.class)
+                .collectList()
+                .block();
 
-            return new ResponseEntity<>(books, HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
+        model.addAttribute("books", books);
 
-    /**
-     * Allows to create a Book
-     *
-     * @param bookRequest
-     * @return
-     */
-    @PostMapping("")
-    public ResponseEntity<Book> create(@RequestBody Book bookRequest) {
-        try {
-
-            Book _book = bookRepository.save(
-                    new Book(
-                            bookRequest.getTitle(),
-                            bookRequest.getPublication_date(),
-                            bookRequest.getPage_number(),
-                            bookRequest.getCategory(),
-                            bookRequest.getBorrow(),
-                            bookRequest.getAuthors()
-                    )
-            );
-
-            return new ResponseEntity<>(_book, HttpStatus.CREATED);
-        } catch (Exception e) {
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
-    /**
-     * Allows to get information about a book
-     *
-     * @param id Book identifier
-     * @return ResponseEntity 200 / 400 / 500
-     */
-    @GetMapping("/{id}")
-    public ResponseEntity<Book> getBookPerId(@PathVariable("id") long id) {
-        try {
-            Optional<Book> bookData = bookRepository.findById(id);
-
-            if (bookData.isPresent()) {
-                return new ResponseEntity<>(bookData.get(), HttpStatus.OK);
-            } else {
-                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-            }
-        } catch (Exception e) {
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        return "book/list";
     }
 }
